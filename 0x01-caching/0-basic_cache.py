b4f0128fe6eaa061e3978eb3669f75662debbdee
niialabi <taufiq.alabi@gmail.com>
2024-04-02 16:21:12 +0000

    basic cache clase implementation

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Contains basic cache class """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ BasicCach class implementation """
+
+    def __init__(self):
+        """ Instantiation """
+        self.cache_data
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key """
+        if not key:
+            return None
+        return self.cache_data.get(key)
