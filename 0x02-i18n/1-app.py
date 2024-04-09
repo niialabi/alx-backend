7db2409349dbe25d66b94e79177cabfe7aafeca2
niialabi <taufiq.alabi@gmail.com>
2024-04-09 22:25:34 +0000

    function name change and syntax cleanup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,16 +8,19 @@ babel = Babel(app)
 
 
 class Config():
-    """ Config glass """
+    """ Config class """
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
-def index():
+def world():
     """ renders default index page via template """
     return render_template('1-index.html')
